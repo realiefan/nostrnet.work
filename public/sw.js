76585140151db162134979f847980060bb2ebfe7
Iefan <66840141+realiefan@users.noreply.github.com>
2023-07-31 17:50:31 +0530

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,36 @@
+// sw.js
+
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = ['/', '/index.html', '/favicon.ico', '/logo192.png', '/logo512.png', '/App.css', '/Modal.js'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
