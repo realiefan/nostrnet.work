773226eeb6c3c2d84c329271fa3442d8d79c7522
Iefan <66840141+realiefan@users.noreply.github.com>
2023-08-02 00:18:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,12 +7,7 @@ import App from './App';
 // Use createRoot from "react-dom/client" instead of ReactDOM.createRoot
 import { createRoot } from 'react-dom/client';
 
-// Register the service worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js');
-  });
-}
+
 
 // Use createRoot instead of ReactDOM.render
 const rootElement = document.getElementById('root');
